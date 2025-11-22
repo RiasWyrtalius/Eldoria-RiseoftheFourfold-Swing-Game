81adba6966ea81935b5425fa4533c109825ca196
Cedrake <epilred@gmail.com>
2025-11-22 19:23:49 +0800

    progress in layout battle arena thingy

--- src/UI/MainInterface/MainInterface.java
+++ src/UI/MainInterface/MainInterface.java
@@ -3,11 +3,15 @@ package UI.MainInterface;
 import Core.LogManager;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainInterface extends JFrame{
     private JPanel contentPanel;
     private JList list1;
     private JTextArea GameLogPanelTextArea;
+    private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
 
     public MainInterface() {
         JFrame frame = new JFrame();
@@ -18,6 +22,5 @@ public class MainInterface extends JFrame{
         setTitle("DND Swing Clone | Saja Boys");
 
         LogManager.initialize(GameLogPanelTextArea);
-        LogManager.log("hello world!");
     }
 }
