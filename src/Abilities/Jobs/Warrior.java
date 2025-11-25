3bc6bd503407a224006909532b58fd47fa390a3b
flurgerburger <samuelcarillo123456@gmail.com>
2025-11-25 17:04:04 +0800

    added idle and attack animation
    for Warrior, Archer, Goblin(no attack animation for Goblin yet)

--- src/Abilities/Jobs/Warrior.java
+++ src/Abilities/Jobs/Warrior.java
@@ -5,6 +5,9 @@ import Abilities.*;
 import Characters.Character;
 import Core.LogColor;
 import Core.LogManager;
+import Core.VisualEffectsManager;
+import Resource.AnimationLoopType;
+import Resource.AssetManager;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -13,29 +16,49 @@ public class Warrior extends JobClass {
 
     public Warrior() {
         super("Warrior", "Wields Sword and Shield", 10, 0);
+        AssetManager.getInstance().registerAnimation(
+                "WARRIOR_IDLE",
+                "Assets/Animations/Warrior/Idle/sprite_%d.png",
+                3, 100, 100 , 300,
+                AnimationLoopType.INFINITE
+        );
+        AssetManager.getInstance().registerAnimation(
+                "WARRIOR_SWORD-SLASH",
+                "Assets/Animations/Warrior/Effects/Sword_Slash/sprite_%d.png",
+                4, 100, 100 , 300,
+                AnimationLoopType.ONE_CYCLE
+        );
+        AssetManager.getInstance().registerAnimation(
+                "WARRIOR_SHIELD-BASH",
+                "Assets/Animations/Warrior/Effects/Shield_Bash/sprite_%d.png",
+                3, 100, 100 , 300,
+                AnimationLoopType.ONE_CYCLE
+        );
     }
 
     public List<Skill> createSkills() {
         FullExecuteConsumer shieldBashLogic = (skill, user, targets, onSkillComplete) -> {
-            int calculateDamage = (user.getBaseAtk() * 2);
+            int calculateDamage = (user.getBaseAtk() * 3);
             Character target = targets.getFirst();
-            target.takeDamage(calculateDamage, user);
-            LogManager.log(skill.getActionLog(user, " Bashes Shield on ", targets, calculateDamage), LogColor.HERO_ACTION);
-
-            if (onSkillComplete != null) {
-                onSkillComplete.run();
-            }
+            LogManager.log(skill.getActionLog(user, "Bashes Shield at", targets, calculateDamage), LogColor.HERO_ACTION);
+            VisualEffectsManager.getInstance().playAnimation("WARRIOR_SHIELD-BASH", user, () -> {
+                target.takeDamage(calculateDamage, user);
+                if (onSkillComplete != null) {
+                    onSkillComplete.run();
+                }
+            }, true);
         };
 
         FullExecuteConsumer swordSlashLogic = (skill, user, targets, onSkillComplete) -> {
-            int calculateDamage = (user.getBaseAtk() * 2);
+            int calculateDamage = (user.getBaseAtk() * 3);
             Character target = targets.getFirst();
-            target.takeDamage(calculateDamage, user);
-            LogManager.log(skill.getActionLog(user, " Slashes on ", targets, calculateDamage), LogColor.HERO_ACTION);
-
-            if (onSkillComplete != null) {
-                onSkillComplete.run();
-            }
+            LogManager.log(skill.getActionLog(user, "Slashes Sword at", targets, calculateDamage), LogColor.HERO_ACTION);
+            VisualEffectsManager.getInstance().playAnimation("WARRIOR_SWORD-SLASH", user, () -> {
+                target.takeDamage(calculateDamage, user);
+                if (onSkillComplete != null) {
+                    onSkillComplete.run();
+                }
+            }, true);
         };
 
         Skill ShieldBash = new Skill(
--- src/Characters/Enemies/Goblin.java
+++ src/Characters/Enemies/Goblin.java
@@ -2,16 +2,35 @@ package Characters.Enemies;
 
 import Characters.Base.Enemy;
 import Characters.Character;
+import Core.VisualEffectsManager;
+import Resource.AnimationLoopType;
+import Resource.AssetManager;
 
 import java.util.List;
 
 public class Goblin extends Enemy {
     public Goblin() {
-        super("Goblin Grunt", 20, 5, 0, 1, "Goblin", 10, "Assets/Images/bstudios.png");
+
+        super("Goblin Grunt", 20, 5, 0, 1, "Goblin", 10, "GOBLIN_IDLE");
+        AssetManager.getInstance().registerAnimation(
+                "GOBLIN_IDLE",
+                "Assets/Animations/Goblin/Idle/sprite_%d.png",
+                4, 100, 100 , 300,
+                AnimationLoopType.INFINITE
+        );
+
+        AssetManager.getInstance().registerAnimation(
+                "GOBLIN_SWING-ATTACK",
+                "Assets/Animations/Goblin/Effects/Swing_Attack/sprite_%d.png",
+                4, 100, 100 , 300,
+                AnimationLoopType.INFINITE
+        );
     }
 
     @Override
     public void makeAttack(List<Character> targets) {
-        targets.getFirst().takeDamage(baseAtk, this);
+
+            targets.getFirst().takeDamage(baseAtk, this);
+
     }
 }
