98f6b85d4de0547b1f986e6b57606642b6de0c67
flurgerburger <samuelcarillo123456@gmail.com>
2025-12-03 21:52:24 +0800

    changed names to cryo and aero mancer

--- src/Abilities/Jobs/CryoMancer.java
+++ src/Abilities/Jobs/CryoMancer.java
@@ -24,7 +24,7 @@ public class CryoMancer extends JobClass {
 
     public CryoMancer(){
 
-        super("Ice Mage", description, 0, 0,"MAGE_ICE-IDLE");
+        super("CryoMancer", description, 0, 0,"MAGE_ICE-IDLE");
 
         AssetManager.getInstance().registerAnimation(
                 "MAGE_ICE-IDLE",
